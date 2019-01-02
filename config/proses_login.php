94b8cf1682bccfbdc5f8a7f65b27dbbaab039ffc
suryow <suryowidiyantogm@gmail.com>
2019-01-02 08:38:29 +0700

    first post

---
+++ config/proses_login.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include 'koneksi.php';
+$email		= $_POST['email'];
+$pass		= $_POST['password'];
+
+if(!empty($email) && !empty($pass)) {
+	$sql		= "SELECT * FROM user WHERE email = '$email' AND password = '$pass'";
+	$query		= mysqli_query($konek, $sql);
+	$row 		= mysqli_fetch_assoc($query);
+	if (mysqli_num_rows($query)>0) {
+		$_SESSION['email']	= $email;
+		$_SESSION['name']	= $row['name'];
+		$_SESSION['photo']	= $row['photo'];
+		header('location: ../admin/index.php');
+	} else {
+		echo "Email anda salah";
+	}
+} else {
+	echo "Email dan password anda kosong";
+}
+
+?>
