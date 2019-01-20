879aa85ec521e92aedf1c0183bca04e9536ee30d
masuryo <suryowidiyantgm@gmail.com>
2019-01-20 23:39:00 +0700

    add photo

--- admin/user/proses_tambah.php
+++ admin/user/proses_tambah.php
@@ -6,17 +6,40 @@ $nama				= $_POST['nama'];
 $email				= $_POST['email'];
 $password			= $_POST['password'];
 
+
+//Input files gambar
+$allowed 			=  array('jpeg','png' ,'jpg');
 $nama_gambar		= $_FILES['gambar']['name'];
 $tmp_name			= $_FILES['gambar']['tmp_name'];
+$size_gambar		= $_FILES['gambar']['size'];
+$ext 				= pathinfo($nama_gambar, PATHINFO_EXTENSION);
 
-$acak				= rand(1111111, 9999999);
-$ubah				= str_replace($nama_gambar, $acak.".jpg", $nama_gambar);
 
-move_uploaded_file($tmp_name, "../../gambar/user-img/".$ubah);
+// echo $size_gambar;
+// exit();
+//alert for undefined extension
+function ext() {
+  alert("Format file anda tidak sesuai!");
+}
 
-$sql = "INSERT INTO user (name, email, password, photo)
-		VALUES ('$nama', '$email', '$password', '$ubah')";
-mysqli_query($konek,$sql);
-header('location:index.php');
+if ((in_array($ext, $allowed)) && ($size_gambar < 10000000) {
+	$acak				= rand(1111111, 9999999);
+	$ubah				= str_replace($nama_gambar, $acak.".jpg", $nama_gambar);
+
+	move_uploaded_file($tmp_name, "../../gambar/user-img/".$ubah);
+
+	$sql = "INSERT INTO user (name, email, password, photo)
+			VALUES ('$nama', '$email', '$password', '$ubah')";
+	mysqli_query($konek,$sql);
+	header('location:index.php');	
+} else {
+	echo "
+			<script type='text/javascript'>
+				alert('file extention not allowed');
+				window.location.href='create.php?file_type_not_allowed_error';
+			</script>
+		";
+}
+
 
 ?>
