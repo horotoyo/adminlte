e9231b21ca8f4210f4e8bb13aa0158b10209bab6
masuryo <suryowidiyantgm@gmail.com>
2019-01-20 15:11:03 +0700

    edit photo profil

--- admin/user/proses_edit.php
+++ admin/user/proses_edit.php
@@ -1,33 +1,37 @@
 <?php
 session_start();
 include '../../config/koneksi.php';
-
 $ID 				= $_POST['id'];
 $nama				= $_POST['nama'];
 $email				= $_POST['email'];
 $password			= $_POST['password'];
 
 //insert gambar
-$regambar			= $_POST['regambar'];
 $nama_gambar		= $_FILES['gambar']['name'];
 $tmp_name			= $_FILES['gambar']['tmp_name'];
 
 //move and rename
-$pindah				= substr($nama_gambar, -4);
-$acak				= rand(11111, 99999);
-$ubah				= str_replace($pindah, $acak.$pindah, $pindah);
+$acak				= rand(1111111, 9999999);
+$ubah				= str_replace($nama_gambar, $acak.".jpg", $nama_gambar);
 move_uploaded_file($tmp_name, "../../gambar/user-img/".$ubah);
-
-//insert to database
 $addto				= "http://localhost/adminlte/gambar/user-img/".$ubah;
 
-$sql1 = "UPDATE user SET
+//Jika tidak mengubah gambar
+$sql1	= "UPDATE user SET 
+			name  		= '$nama',
+			email 		= '$email',
+			password 	= '$password'
+			WHERE id 	= '$ID'";
+
+//Jika mengubah gambar
+$sql2	= "UPDATE user SET 
 			name 		= '$nama',
 			email 		= '$email',
-			password	= '$password',
-			photo		= '$regambar'
-			WHERE id 	= '$ID'"; //jika kosong
+			password 	= '$password',
+			photo		= '$addto'
+			WHERE id 	= '$ID'";
 
+//Ambil Data
 $ambil		= "SELECT * FROM user where id='$ID'";
 $hasil   	= mysqli_query($konek, $ambil);
 $row	   	= mysqli_fetch_assoc($hasil);
@@ -35,22 +39,21 @@ $hapus		= $row['photo'];
 $awal		= substr($hapus, 42);
 $del 		= "../../gambar/user-img/".$awal;
 
-$sql2 = "UPDATE user SET
-			name 		= '$nama',
-			email 		= '$email',
-			password	= '$password',
-			photo		= '$hapus'
-			WHERE id 	= '$ID'"; //jika diisi
-// mysqli_query($konek,$sql);
-// header('location:index.php');
 
-if (!isset($nama_gambar)) {
-	mysqli_query($konek,$sql1);
+if (unlink($del)) {
+	mysqli_query($konek,$sql2);
 	header('location:index.php');
 } else {
-	mysqli_query($konek,$sql2);
-	unlink($del);
+	mysqli_query($konek,$sql1);
 	header('location:index.php');
 }
 
+// if (isset($nama_gambar)) {
+// 	mysqli_query($konek,$sql1);
+// 	header('location:index.php');
+// } else {
+// 	unlink($del);
+// 	mysqli_query($konek,$sql2);
+// 	header('location:index.php');
+// }
 ?>
